d9e4dc01b50d94bb7fa020673ad69aecc550f205
hugogamer123 <81802978+hugogamer123@users.noreply.github.com>
2025-12-23 21:28:52 +0100

    started prototype of game
    Started developing a prototype of the game, where you select genre, select moreoptions, get a rating and stuff

---
+++ Assets/Script/GDT GameCreationPrototype/MenuTransitionAnim.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class MenuTransitionAnim : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+}
---
+++ Assets/Script/GDT GameCreationPrototype/OpenMenu.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class OpenMenu : MonoBehaviour
+{
+    public GameObject gameUI;
+
+    public void Awake()
+    {
+        gameUI.SetActive(false);
+    }
+
+    public void Update()
+    {
+        CheckForInput();
+    }
+
+    private void CheckForInput()
+    {
+        //Mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if(hit.collider.CompareTag("Player"))
+                {
+                    Debug.Log("Clicked on: " + hit.collider.name);
+                    gameUI.SetActive(!gameUI.activeSelf);
+                }
+            }
+        }
+    }
+}
